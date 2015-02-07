fa3efb73fa0a28e1f5a16e881a538fad9794a9cc
Michael Husmann <michaelhusmann@gmail.com>
2015-02-07 18:04:30 +0100

    added gnuplot output

---
+++ README.md
@@ -0,0 +1,6 @@
+heatresults 2015-02-07 mih
+
+generate reports from my heatpump system.
+Written with google go using a sqlite database and plotting the data
+with gnuplot by request
+
--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 /*
  * Auswertung meiner Heatpump Datenbank 2014-12-12 mih
+ * letzte Änderung Fr 23. Jan 11:58:03 CET 2015
  *
- * TODO
  * Gesamtübersicht: alle Werte insgesamt -- wenn weder Jahr noch Monat angegeben sind
  * Jahresübersicht: alle Werte eines Jahres, Beispiel -y 2010
  * Monatsübersicht: Werte eines Monats, -y 2010 -m 2
@@ -11,31 +11,135 @@ package main
  * Monate/Jahr:     Nur die Summen der Monate eines Jahres, -y 2014 -sum
  * Monate/Jahre:    Die Monatssummen aller Jahre / inkl. Jahresausgabe -alls
  *
- * Alle Ausgaben auch in CVS Dateien
+ * Alle Ausgaben auch in CSV Dateien
  */
 
 import (
 	"code.google.com/p/go-sqlite/go1/sqlite3"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
-// const HeatpumpDB = "/home/mhusmann/usr/src/gocode/src/github.com/mhusmann/heatresults/heatpump.db"
-const HeatpumpDB ="/home/mhusmann/Documents/src/pyt/heizung/heatpump.db"
-const HeaderLine = "Jahr/Monat;\tJan;\tFeb;\tMar;\tApr;\tMai;\tJun;\tJul;\t" +
-	"Aug;\tSep;\tOct;\tNov;\tDec"
+const pltSt1 = `
+## set terminal pbm color
+##set output "statistic.pbm"
+set term qt persist
+set title "Verbrauchswerte"
+## set xrange [1:31]
+set xlabel "Datum"; set ylabel "kWh"
+set xtics rotate by -30
+#set key top left
+set key autotitle columnhead
+set boxwidth 0.6
+set style fill solid 0.4 border
+set grid
+#set xtics ("Jan" 1, "Feb" 2, "Mar" 3, "Apr" 4, "May" 5,\
+#            "Jun" 6, "Jul" 7, "Aug" 8, "Sep" 9, "Oct" 10,\
+#            "Nov" 11, "Dec" 12)
+`
+const heatpumpDB = "/home/mhusmann/Documents/src/pyt/heizung/heatpump.db"
+const headerLine = "Jahr/Monat\tJan\tFeb\tMar\tApr\tMai\tJun\tJul\t" +
+	"Aug\tSep\tOct\tNov\tDec"
 
-type Row struct {
-	Id    int
-	Date  string
-	Ht    int
-	Nt    int
-	HtNt  int
-	Tarif int
+var theMonths = [12]string{"Jan", "Feb", "Mar", "Apr", "Mai",
+	"Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+
+type row struct {
+	id    int
+	date  string
+	ht    int
+	nt    int
+	htnt  int
+	tarif int
 }
 
+type plot struct {
+	datName string
+	pltName string
+	datFile *os.File
+	pltFile *os.File
+}
+
+func (p *plot) init(requestedDate string, pltRest int) {
+	var err error
+
+	p.pltName = strings.Replace(requestedDate, "%", "", -1) + ".plt"
+	p.pltFile, err = os.Create(p.pltName)
+	if err != nil {
+		panic(err)
+	}
+
+	p.datName = strings.Replace(requestedDate, "%", "", -1) + ".dat"
+
+	p.datFile, err = os.Create(p.datName)
+	if err != nil {
+		panic(err)
+	}
+
+	if pltRest < 2 {
+		_, err := io.WriteString(p.datFile, "the first line\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	_, err = io.WriteString(p.pltFile, pltSt1)
+	if err != nil {
+		panic(err)
+	}
+
+	var pltSt string
+	switch pltRest {
+	case 0:
+		pltSt = fmt.Sprintf(` plot '%s' using 3:xtic(2) t " HT " with boxes lt 7,\
+    			'' u 4 t " NT " with linesp lt 3,\
+    			'' u 5 t " HT+NT " with linespoints lt 4`, p.datName)
+
+	case 1:
+		pltSt = fmt.Sprintf(` plot '%s' using 2:xtic(1) t " HT " with boxes lt 7,\
+    			'' u 3 t " NT " with linesp lt 3,\
+    			'' u 4 t " HT+NT " with linespoints lt 4`, p.datName)
+
+	case 2:
+		pltSt = fmt.Sprintf(` plot '%s' using 2:xtic(1) with linesp lt 1,\
+    			'' u 3  w linesp lt 3,\
+    			'' u 4  w linesp lt 4,\
+    			'' u 5  w linesp lt 5,\
+    			'' u 6  w linesp lt 6,\
+    			'' u 7  w linesp lt 7,\
+    			'' u 8  w linesp lt 8`, p.datName)
+	}
+
+	_, err = io.WriteString(p.pltFile, pltSt)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (p *plot) close() {
+	fmt.Println("# closing plot files")
+	p.datFile.Close()
+	p.pltFile.Close()
+}
+
+func (p *plot) writeDat(st string) {
+	_, err := io.WriteString(p.datFile, st)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (p *plot) gnuplt(pltName string) {
+	_, err := exec.Command("gnuplot", p.pltName).Output()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func monthlyValues(conn *sqlite3.Conn, year string, month int) (ht, nt, htnt int, average float64) {
 	m := fmt.Sprintf("%02d", month)
@@ -44,8 +148,8 @@ func monthlyValues(conn *sqlite3.Conn, year string, month int) (ht, nt, htnt int
 	var noOfDays int
 	stmt := fmt.Sprintf("SELECT COUNT(day), MAX(day) FROM dayly WHERE day LIKE %q",
 		year+"-"+m+"%")
-	for s, err := conn.Query(stmt); err == nil; err = s.Next() {
-		s.Scan(&noOfDays, &lastDate)
+	for dbResult, err := conn.Query(stmt); err == nil; err = dbResult.Next() {
+		dbResult.Scan(&noOfDays, &lastDate)
 	}
 
 	if lastDate == "2009-04-30" {
@@ -61,22 +165,28 @@ func monthlyValues(conn *sqlite3.Conn, year string, month int) (ht, nt, htnt int
 			"WHERE d.day = %q", lastDate)
 	}
 
-	for s, err := conn.Query(stmt); err == nil; err = s.Next() {
-		s.Scan(&ht, &nt, &htnt)
+	for dbResult, err := conn.Query(stmt); err == nil; err = dbResult.Next() {
+		dbResult.Scan(&ht, &nt, &htnt)
 	}
 
 	average = float64(htnt) / float64(noOfDays)
 	return ht, nt, htnt, average
 }
 
-func sumYear(conn *sqlite3.Conn, year string) {
+func sumYear(conn *sqlite3.Conn, g bool, year string) {
 	// request min and max month of the year from database
+	p := new(plot)
+	if g {
+		p.init(year, 1)
+		defer p.close()
+	}
+
 	stmt := fmt.Sprintf("SELECT strftime(%q, MIN(day)), strftime(%q, MAX(day)) "+
 		"FROM dayly WHERE day LIKE %q", "%m", "%m", year+"%")
 
 	var startMonth, endMonth string
-	for s, err := conn.Query(stmt); err == nil; err = s.Next() {
-		s.Scan(&startMonth, &endMonth)
+	for dbResult, err := conn.Query(stmt); err == nil; err = dbResult.Next() {
+		dbResult.Scan(&startMonth, &endMonth)
 	}
 
 	fmt.Println("# Jahresübersicht: ", year)
@@ -87,16 +197,32 @@ func sumYear(conn *sqlite3.Conn, year string) {
 	endm, _ := strconv.Atoi(endMonth)
 	for i, _ := strconv.Atoi(startMonth); i <= endm; i++ {
 		ht, nt, htnt, avg = monthlyValues(conn, year, i)
-		fmt.Printf("%4d;%5d;%5d;%5d;%6.2f\n", i, ht, nt, htnt, avg)
+		outSt := fmt.Sprintf("%4s %5d %5d %5d %6.2f\n", theMonths[i-1], ht, nt, htnt, avg)
+		fmt.Printf(outSt)
+		if g {
+			p.writeDat(outSt)
+		}
 		sumHt += ht
 		sumNt += nt
 		sumHtNt += htnt
 	}
 	fmt.Printf("# Totals: Ht %3d, Nt %3d, HtNt %3d\n", sumHt, sumNt, sumHtNt)
+
+	if g {
+		p.gnuplt(p.pltName)
+	}
 }
 
-func theRest(conn *sqlite3.Conn, requestedDate string) {
+func theRest(conn *sqlite3.Conn, g bool, requestedDate string) {
 	fmt.Println("# requestedDate", requestedDate)
+	p := new(plot)
+	if g {
+		p.init(requestedDate, 0)
+		defer p.close()
+	}
+
+	sumHt, sumNt, sumHtNt, countDays := 0, 0, 0, 0
+	var aRow row
 
 	stmt := fmt.Sprintf("select d.id, d.day 'day', d.ht-p.ht HT, d.nt-p.nt NT, "+
 		"d.ht-p.ht + d.nt-p.nt 'HT+NT' "+
@@ -105,19 +231,24 @@ func theRest(conn *sqlite3.Conn, requestedDate string) {
 		"on date(d.day,'-1 day') = p.day "+
 		"where d.day like %q", requestedDate)
 
-	sumHt, sumNt, sumHtNt, countDays := 0, 0, 0, 0
-	var aRow Row
-
-	for s, err := conn.Query(stmt); err == nil; err = s.Next() {
-		s.Scan(&aRow.Id, &aRow.Date, &aRow.Ht, &aRow.Nt, &aRow.HtNt)
-		fmt.Printf("%6d\t%s\t%3d\t%3d\t%3d\n", aRow.Id, aRow.Date, aRow.Ht, aRow.Nt, aRow.HtNt)
-		sumHt += aRow.Ht
-		sumNt += aRow.Nt
-		sumHtNt += aRow.HtNt
-		countDays += 1
+	for dbResult, err := conn.Query(stmt); err == nil; err = dbResult.Next() {
+		dbResult.Scan(&aRow.id, &aRow.date, &aRow.ht, &aRow.nt, &aRow.htnt)
+		outSt := fmt.Sprintf("%6d\t%s\t%3d\t%3d\t%3d\n", aRow.id, aRow.date, aRow.ht, aRow.nt, aRow.htnt)
+		fmt.Printf(outSt)
+		if g {
+			p.writeDat(outSt)
+		}
+		sumHt += aRow.ht
+		sumNt += aRow.nt
+		sumHtNt += aRow.htnt
+		countDays++
 	}
 	fmt.Printf("# Totals: HT: %d, NT: %d, HT+NT: %d, days: %d, avg/day: %3.2f kWh\n",
 		sumHt, sumNt, sumHtNt, countDays, float64(sumHtNt)/float64(countDays))
+
+	if g {
+		p.gnuplt(p.pltName)
+	}
 }
 
 func total(conn *sqlite3.Conn, month, year int) int {
@@ -136,54 +267,79 @@ func total(conn *sqlite3.Conn, month, year int) int {
 	}
 
 	var htnt int
-	for s, err := conn.Query(stmt); err == nil; err = s.Next() {
-		s.Scan(&htnt)
+	for dbResult, err := conn.Query(stmt); err == nil; err = dbResult.Next() {
+		dbResult.Scan(&htnt)
 	}
 	return htnt
 }
 
-func allSums(conn *sqlite3.Conn) {
-	stmt := fmt.Sprintf(`select strftime(%q, min(day)), strftime(%q, min(day)), `+
+func allSums(conn *sqlite3.Conn, g bool) {
+	p := new(plot)
+	if g {
+		p.init("all-sums", 2)
+		defer p.close()
+	}
+
+	// storing the results in a map. Year as key the months values as a slice
+	var data = make(map[string][]string)
+
+	stmt := fmt.Sprintf(`select strftime(%q, min(day)), `+
+		`strftime(%q, min(day)), `+
 		`strftime(%q, max(day)), strftime(%q, max(day)) `+
 		`from dayly`, "%m", "%Y", "%m", "%Y")
 	var startMonth, startYear, endMonth, endYear string
-	for s, err := conn.Query(stmt); err == nil; err = s.Next() {
-		s.Scan(&startMonth, &startYear, &endMonth, &endYear)
+	for dbResult, err := conn.Query(stmt); err == nil; err = dbResult.Next() {
+		dbResult.Scan(&startMonth, &startYear, &endMonth, &endYear)
 	}
-	startM, _ := strconv.Atoi(startMonth)
+
+	startM := 1
 	startY, _ := strconv.Atoi(startYear)
-	endM, _ := strconv.Atoi(endMonth)
 	endY, _ := strconv.Atoi(endYear)
 
-	fmt.Println(HeaderLine)
-	var em int
+	fmt.Println(headerLine)
+	em := 12
 	for sy := startY; sy <= endY; sy++ {
-		if sy < endY {
-			em = 12
-		} else {
-			em = endM
-		}
-		fmt.Printf("%8d;", sy)
+		sySt := fmt.Sprint(sy)
+		fmt.Printf("%8d", sy)
 		for i := 1; i < startM; i++ {
-			fmt.Printf("\t;")
+			fmt.Printf("\t")
 		}
 		for sm := startM; sm <= em; sm++ {
-			fmt.Printf("\t%3d;", total(conn, sm, sy))
+			totals := total(conn, sm, sy)
+			data[sySt] = append(data[sySt], fmt.Sprint(totals))
+			fmt.Printf("\t%3d", totals)
 		}
 		startM = 1
 		fmt.Println()
+	}
+
+	if g {
+		p.writeDat(fmt.Sprintf("%d\t", 0))
+		for year := startY; year <= endY; year++ {
+			p.writeDat(fmt.Sprintf("%d\t", year))
+		}
+		p.writeDat("\n")
+		for i := 1; i <= 12; i++ {
+			p.writeDat(fmt.Sprintf("%s\t", theMonths[i-1]))
+			for year := startY; year <= endY; year++ {
+				p.writeDat(fmt.Sprintf("%s\t", data[fmt.Sprint(year)][i-1]))
+			}
+			p.writeDat("\n")
+		}
+		p.gnuplt(p.pltName)
 	}
 }
 
 func main() {
 	var db, y, m, d string
-	var sum, alls bool
-	flag.StringVar(&db, "db", HeatpumpDB, "Pfad/Name der Datenbank")
+	var sum, alls, g bool
+	flag.StringVar(&db, "db", heatpumpDB, "Pfad/Name der Datenbank")
 	flag.StringVar(&y, "y", "", "zu berechnendes Jahr")
 	flag.StringVar(&m, "m", "", "Monat")
 	flag.StringVar(&d, "d", "", "Tag")
 	flag.BoolVar(&sum, "sum", false, "Nur Summe")
 	flag.BoolVar(&alls, "alls", false, "Summen aller Jahre als Übersicht")
+	flag.BoolVar(&g, "g", false, "HTML Datei für graphische Übersicht")
 	flag.Parse()
 
 	fmt.Println("# Datenbank:", db)
@@ -194,23 +350,24 @@ func main() {
 	fmt.Println("# Alle Summen:", alls)
 	fmt.Println("# tail:", flag.Args())
 
-	conn, err := sqlite3.Open(HeatpumpDB)
+	conn, err := sqlite3.Open(heatpumpDB)
 	if err != nil {
-		fmt.Println("# kann Datenbank %s nicht öffnen: %s", HeatpumpDB, err)
+		fmt.Println("# kann Datenbank %s nicht öffnen: %s",
+			heatpumpDB, err)
 		os.Exit(1)
 	}
 	defer conn.Close()
 
 	switch {
 	default:
-		theRest(conn, y+"%")
+		theRest(conn, g, y+"%")
 	case alls == true:
-		allSums(conn)
+		allSums(conn, g)
 	case sum == true:
-		sumYear(conn, y)
+		sumYear(conn, g, y)
 	case m != "" && d != "":
-		theRest(conn, y+"-"+m+"-"+d)
+		theRest(conn, g, y+"-"+m+"-"+d)
 	case m != "":
-		theRest(conn, y+"-"+m+"%")
+		theRest(conn, g, y+"-"+m+"%")
 	}
 }
